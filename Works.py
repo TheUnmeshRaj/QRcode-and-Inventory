926c7d9fe26e4f4a653f188618c90383d81e01cf
TheUnmeshRaj <114986695+TheUnmeshRaj@users.noreply.github.com>
2024-06-28 22:46:27 +0530

    added visualize tab in the new navigation bar

--- Works.py
+++ Works.py
@@ -1,10 +1,9 @@
-import threading
-
-import cv2
-import requests
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+import threading
 from pyzbar.pyzbar import decode
+import cv2
+import requests
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -17,39 +16,40 @@ def send_qr_code_to_server(data):
 
 def scan_qr_codes():
     cap = cv2.VideoCapture(0)
-    
-
     while True:
         ret, frame = cap.read()
         if not ret:
             break
         decoded_objects = decode(frame)
-        
         for obj in decoded_objects:
             qr_data = obj.data.decode('utf-8')
             if qr_data in detected_qr_codes_set:
                 continue
             detected_qr_codes_set.add(qr_data)
             send_qr_code_to_server(qr_data)
-            socketio.emit('new_qr_code', qr_data)  # Update clients with new QR code
+            socketio.emit('new_qr_code', qr_data)
         socketio.emit('detected_qr_codes', list(detected_qr_codes_set))
         cv2.imshow('QR Code Scanner', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
     cv2.destroyAllWindows()
-    
+
 @app.route('/')
 def index():
-    return render_template('index.html', qr_codes=list(detected_qr_codes_set))
+    return render_template('index.html')
 
 @app.route('/submit_qr_code', methods=['POST'])
 def submit_qr_code():
     data = request.form['data']
     if data not in detected_qr_codes_set:
         detected_qr_codes_set.add(data)
-        socketio.emit('new_qr_code', data)  
+        socketio.emit('new_qr_code', data)
     return 'OK', 200
+
+@app.route('/visualize')
+def visualize():
+    return render_template('visualize.html')
 
 if __name__ == '__main__':
     threading.Thread(target=scan_qr_codes).start()
